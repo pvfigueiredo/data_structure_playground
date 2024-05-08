87fda64f54c9c132ca87918a0d45510da18d0732
pvfigueiredo <paulo.figueiredo86@gmail.com>
2024-05-08 15:45:33 -0300

    Removed unnecessary files

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from sort_algorithm import *
--- tests/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append("c:\\Dev\\python\\merge_sort\\sort_algorithm")
-
-print(sys.path)
-
-from sort_algorithm.merge_sort import *
-
-print(MergeSort.sort([2,1]))
